df062a5971b6d7e6bb4f61eab16cbbd65f24d112
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2025-06-26 17:51:22 -0300

    Agregar logica contador

--- app/counter.tsx
+++ app/counter.tsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function CounterScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Counter screen</Text>
-    </View>
-  );
+    const [count, setCount] = useState(0);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Counter</Text>
+            <Text style={styles.count}>{count}</Text>
+            <View style={styles.buttonGroup}>
+                <Button title='+' onPress={() => setCount(count + 1)} />
+                <Button title='-' onPress={() => setCount(count - 1)} />
+                <Button title='Reset' onPress={() => setCount(0)} />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -14,7 +23,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
+  title: {
+    fontSize: 32,
     color: '#000',
+    marginBottom: 20
   },
+  count: {
+    fontSize: 48,
+    color: '#333',
+    marginBottom: 20
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    gap: 10
+  }
 });
